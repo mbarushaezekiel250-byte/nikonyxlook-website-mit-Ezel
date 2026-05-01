f1505fe391c57b2cc39a881e4a2d5b8f067c2161
Ezel311 <mbarushaezekiel250@gmail.com>
2026-05-01 13:32:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("startBtn").addEventListener("click", function(){
+    alert("Feature coming soon: Smart Outfit Generator!");
+});
+
+function goHome(){
+    window.location.href = "../index.html";
+}
